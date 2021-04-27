597c849a6d25eeb81fbc72bd2bb81ed49675806f
Anshul Tomar <decoder746@github.com>
2021-04-28 01:14:33 +0530

    Variant 1,2

--- Synthetic/all.sh
+++ Synthetic/all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python main_bilevel.py --joint --scale 100 --change_prob 0.25 --exp_name s-0.1-joint  &
-python main_bilevel.py --joint --scale 100 --change_prob 0.5 --exp_name s-1-joint &
-python main_bilevel.py --joint --scale 100 --change_prob  0.75 --exp_name s-10-joint &
-python main_bilevel.py --joint --scale 100 --change_prob  1 --exp_name s-50-joint 
+python main_bilevel.py --joint --scale 100 --change_prob 0.25 --exp_name v1-0.25-joint  &
+python main_bilevel.py --joint --scale 100 --change_prob 0.5 --exp_name v1-0.5-joint &
+python main_bilevel.py --joint --scale 100 --change_prob  0.75 --exp_name v1-0.75-joint &
+python main_bilevel.py --joint --scale 100 --change_prob  1 --exp_name v1-1-joint 
