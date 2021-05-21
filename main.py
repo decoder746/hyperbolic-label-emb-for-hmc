7176eaeb5e93ef03b56f2355a5ac06190013c84f
Anshul Tomar <decoder746@github.com>
2021-05-21 17:48:16 +0530

    A small mistake

--- main.py
+++ main.py
@@ -319,13 +319,13 @@ def train_bilevel(epochs, trainloader, valloader, testloader, combinedmodel, arg
             del wt_grads
             optimizer.zero_grad()
             dot, label_edges = combinedmodel(docs, Y, edges)
-            labels_pop = labels.sum(axis=0)/(1.0*labels.sum())
+            # labels_pop = labels.sum(axis=0)/(1.0*labels.sum())
             if args_model_init["joint"]:
                 losses, geo_loss, _ = criterion(dot, labels, label_edges)
-                loss = torch.dot(losses*labels_pop, weights[:-1]) + weights[-1]*geo_loss
+                loss = torch.dot(losses, weights[:-1]) + weights[-1]*geo_loss
             else:
                 losses, _ = criterion(dot, labels, label_edges)
-                loss = torch.dot(losses*labels_pop, weights)
+                loss = torch.dot(losses, weights)
             total_loss += loss.item()
             loss.backward()
             optimizer.step()
